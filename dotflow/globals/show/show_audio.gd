49472d1e931961f18faa4890044d04b158ab33d7
noah <noah@noahw.design>
2024-08-29 19:43:37 -0400

    null check on audio file

--- dotflow/globals/show/show_audio.gd
+++ dotflow/globals/show/show_audio.gd
@@ -2,13 +2,15 @@ class_name DotFlowShowAudio
 extends Node
 
 var audio_track: AudioStreamMP3 = AudioStreamMP3.new()
-var audio_path = "E:/SteamLibrary/steamapps/music/Frostpunk Original Soundtrack/Frostpunk Original Soundtrack-Normal/Piotr Musiał - Frostpunk (Original Soundtrack) - 09 - The City Must Survive.mp3"
+@export var audio_path = "/home/Noah/Desktop/Piotr Musiał - Frostpunk (Original Soundtrack) - 09 - The City Must Survive.mp3"
 
 
 func _ready():
 	var file = FileAccess.open(audio_path, FileAccess.READ)
+	if file == null:
+		OS.alert("No Audio Track Selected For Playback.", "No Audio Track")
+		return
 	audio_track.data = file.get_buffer(file.get_length())
-
 
 func get_audio_track_length() -> float:
 	return audio_track.get_length()
